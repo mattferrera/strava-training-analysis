1afb5b4a5a6f1c8439f49b17ca8aa427f20cf7e1
Matt Ferrera <44503689+mattferrera@users.noreply.github.com>
2020-11-13 08:42:22 -0600

    Add files via upload
    Client ID, API secret and secret access token config.py file

---
+++ config.py
@@ -0,0 +1,3 @@
+client_id = 'YOUR_CLIENT_ID' # retrieve from https://www.strava.com/settings/api
+api_secret = 'YOUR_API_SECRET' # retrieve from https://www.strava.com/settings/api
+access_token = 'YOUR_ACCESS_TOKEN' # need to update this
